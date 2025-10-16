00183000707e2b2cbf6e6e228928a58ac67f40db
newtondotcom <asphalt9fr@gmail.com>
2025-10-16 23:11:12 +0200

    Initial Commit

---
+++ poky/pokyApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokyApp.swift
+//  poky
+//
+//  Created by Robin Augereau on 16/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct pokyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
